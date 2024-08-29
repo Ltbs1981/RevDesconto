60fce2734c4729e3c96eee198b6cb7da8f82ca61
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-08-29 14:26:20 -0300

    pronto

---
+++ Desconto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevDesconto
+{
+    public class Desconto
+    {
+        public string Nome;
+        public int Quantidade;
+        public double Preco;
+
+        public Desconto(string nome, int quantidade, double preco)
+        {
+            Nome = nome;
+            Quantidade = quantidade;
+            Preco = preco;
+        }
+        public double CalculandoDesconto()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RevDesconto;
+class Program
+{
+    static void Main(string[] args) 
+    {
+        Desconto desconto = new Desconto("IPhone 12", 20, 4000);
+
+        double valor = desconto.CalculandoDesconto();
+        Console.WriteLine($"Você está comprando: \n Produto: {desconto.Nome}. \n Quantidades: {desconto.Quantidade} .\n Valor unitário: R$ {desconto.Preco}.\n Total sem desconto: R$ {valor}\n \n");
+        
+        if(desconto.Quantidade >= 11 && desconto.Quantidade<= 20)
+        {
+            valor -= 0.10 * valor;
+            Console.WriteLine($"Devido ao seu desconto por ter comprado mais de 10 unidades,\n Pagará R$ {valor} ");
+        }else if (desconto.Quantidade >= 21 && desconto.Quantidade <= 50)
+        {
+            valor -= 0.20 * valor;
+            Console.WriteLine($"Devido ao seu desconto por ter comprado mais de 20 unidades,\n Pagará R$ {valor} ");
+        }else if (desconto.Quantidade > 50 )
+        {
+            valor -= 0.25 * valor;
+            Console.WriteLine($"Devido ao seu desconto por ter comprado mais de 50 unidades,\n Pagará R$ {valor} ");
+        }
+
+
+
+    }
+}
